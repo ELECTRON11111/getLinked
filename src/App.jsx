1c5bbd53c819bab4261d8a25ad945ef44d76b348
daniel_eln <omoniyiopemipo6@gmail.com>
2023-09-25 18:01:10 +0100

    sideBar toggle functionality is now active

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import './App.css';
 function App() {
 
   return (
-
     <div className='App'>
       {/* This App component represents the home page */}
       <Layout />
--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import NavBar from "../NavBar/navBar";
 import SideBar from "../../components/SideBar/sideBar";
 
 const Layout = () => {
+    // show is boolean.
+    const [show, changeShow] = useState(false);
+
+    function drawerOpenHandler() {
+        console.log(show);
+        // ensure state immutability
+        const newShowValue = true;
+        changeShow(newShowValue);
+    }
+
+
+    function drawerCloseHandler() {
+        console.log(show);
+        // ensure state immutability
+        const newShowValue = false;
+        changeShow(newShowValue);
+    }
+
     return (
         // This is a hoc(Higher Order Component)
         <>
-            <NavBar />
-            <SideBar />
+            <NavBar clicked = {drawerOpenHandler}/>
+            <SideBar open = {show} cancelClicked = {drawerCloseHandler}/>
         </>
     )
 }
